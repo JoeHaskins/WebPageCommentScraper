2513025b649cae6304c4305d004175223e31f61d
JoeHaskins <joeshaskins@gmail.com>
2021-02-21 04:57:35 +0000

    Read in urls from .txt and scrape page

---
+++ main.py
@@ -0,0 +1,20 @@
+#
+import requests, bs4, re, time
+
+lines = []
+i=0
+
+urlsfilepath = input("Path for .txt of urls:")
+with open(urlsfilepath) as file:
+    lines = file.readlines()
+
+for line in lines:
+    pagedata = requests.get(line)
+    cleanpagedata = bs4.BeautifulSoup(pagedata.text, 'html.parser')
+
+    stringpagedata = re.sub('\n', ' ', str(cleanpagedata))
+    comment = re.findall('<!--.*-->', stringpagedata)
+
+
+    print(comment)
+    print(stringpagedata)
