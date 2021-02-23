81c7344e93a6d51accefeefab93f057e96198b82
Joe Haskins <joe.h@arkham.dub>
2021-02-23 18:04:36 +0000

    Error handling and cla

--- main.py
+++ main.py
@@ -2,41 +2,60 @@
 import bs4
 import re
 import requests
+import argparse
+import sys
 from datetime import datetime
 
-urlsfilepath = input("Path for .txt of urls:")
-with open(urlsfilepath) as file:
-    lines = file.readlines()
+parser = argparse.ArgumentParser(description='Process a list of urls and scrape comments.')
+parser.add_argument('file_path', help='Provide file path of .txt with list of urls to check',
+                    nargs='?', default='0')
+args = parser.parse_args()
+print(args)
 
-    current_datetime = datetime.now()
-    name = str(current_datetime).replace(':', '_')
-    name = name.replace('.', '_')
-    name = name + ".txt"
+if args.file_path != '0':
 
-    f = open(name, "w+")
+    try:
+        with open(args.file_path) as file:
+            lines = file.readlines()
 
-for line in lines:
-    url = re.sub('\n', '', str(line))
-    pagedata = requests.get(url)
-    cleanpagedata = bs4.BeautifulSoup(pagedata.text, 'html.parser')
+            current_datetime = datetime.now()
+            name = str(current_datetime).replace(':', '_')
+            name = name.replace('.', '_')
+            name = name + ".txt"
 
-    stringpagedata = re.sub('\n', ' ', str(cleanpagedata))
-    htmlcomments = re.findall(r'<!--(.+?)-->', stringpagedata)
-    jscomments = re.findall(r'/\*(.+?)\*/', stringpagedata)
-    singlejscomments = re.findall(r'\B//(.+?)\n', str(cleanpagedata))
+            f = open(name, "w+")
 
-    print(htmlcomments)
-    print(jscomments)
-    print(singlejscomments)
+        for line in lines:
+            url = re.sub('\n', '', str(line))
+            pagedata = requests.get(url)
+            if pagedata.status_code == 200:
+                cleanpagedata = bs4.BeautifulSoup(pagedata.text, 'html.parser')
 
-    f.write("--"+url+"\n\n")
-    for i in htmlcomments:
-        f.write(str(i)+"\n")
-    for i in jscomments:
-        f.write(str(i)+"\n")
-    for i in singlejscomments:
-        f.write(str(i)+"\n")
-    f.write("\n\n\n")
+                stringpagedata = re.sub('\n', ' ', str(cleanpagedata))
+                htmlcomments = re.findall(r'<!--(.+?)-->', stringpagedata)
+                jscomments = re.findall(r'/\*(.+?)\*/', stringpagedata)
+                singlejscomments = re.findall(r'\B//(.+?)\n', str(cleanpagedata))
 
-f.close()
+                print(htmlcomments)
+                print(jscomments)
+                print(singlejscomments)
 
+                f.write("--"+url+"\n\n")
+                for i in htmlcomments:
+                    f.write(str(i)+"\n")
+                for i in jscomments:
+                    f.write(str(i)+"\n")
+                for i in singlejscomments:
+                    f.write(str(i)+"\n")
+                f.write("\n\n\n")
+            else:
+                print("An error occurred with URL: "+url+" HTTP Status Code: "+str(pagedata.status_code))
+
+        f.close()
+    except:
+        print("Bad file path")
+        sys.exit()
+
+
+else:
+    print(FileNotFoundError)
